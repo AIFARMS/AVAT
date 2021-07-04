7e159fd56f59bb69760d692d0dc53865afa3d679
Pradeep Senthil <pradeepsen99@gmail.com>
2021-07-04 13:18:37 -0400

    Updates

--- precache-manifest.270b7c7e8ab3746d69e641f447836423.js
+++ precache-manifest.270b7c7e8ab3746d69e641f447836423.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "18dfbf42560e5437e15a02afbcf3a285",
+    "revision": "657994b3e4dd50eb37e5ea2f7fa6d620",
     "url": "/pig-annotation-tool/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/pig-annotation-tool/static/css/2.cf23c88f.chunk.css"
   },
   {
-    "revision": "3bf3f1f241ef9fa34e2b",
+    "revision": "81274e31f86bd8760fd3",
     "url": "/pig-annotation-tool/static/css/main.7f904051.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/pig-annotation-tool/static/js/2.fcf17aea.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3bf3f1f241ef9fa34e2b",
-    "url": "/pig-annotation-tool/static/js/main.0fa474ee.chunk.js"
+    "revision": "81274e31f86bd8760fd3",
+    "url": "/pig-annotation-tool/static/js/main.3fada48a.chunk.js"
   },
   {
     "revision": "adfa5934224b12434833",
