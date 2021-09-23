03f189053b37f665523473157aea995260f8ceb4
pradeepsen99 <pradeepsen99@users.noreply.github.com>
2021-09-23 19:08:43 +0000

    deploy: 3dd305c9844f6923702bee2f5cdb4fbcc3df7843

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/AVAT/precache-manifest.83891a7efe67aa1269fe2af7b70f64cc.js"
+  "/AVAT/precache-manifest.a68a7674b687dbe90c189dddb11940c3.js"
 );
 
 self.addEventListener('message', (event) => {
