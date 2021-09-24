3f6aad3cae4d3adae745a67cf9f0bc4d2d879848
pradeepsen99 <pradeepsen99@users.noreply.github.com>
2021-09-24 23:33:16 +0000

    deploy: e5e2163d64bb45751cd9c63fd43b01a93e3f7248

---
+++ precache-manifest.03c5388f9326536b16cf8a5cce420c8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "928aa45d28177c4d30e9066d378ce701",
+    "url": "/AVAT/index.html"
+  },
+  {
+    "revision": "581078be9d15264c314d",
+    "url": "/AVAT/static/css/2.0d2fdb4f.chunk.css"
+  },
+  {
+    "revision": "49cc66b992079400e5e2",
+    "url": "/AVAT/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "581078be9d15264c314d",
+    "url": "/AVAT/static/js/2.df70773d.chunk.js"
+  },
+  {
+    "revision": "6e28879f31e445a9e15ed81f1edbde57",
+    "url": "/AVAT/static/js/2.df70773d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49cc66b992079400e5e2",
+    "url": "/AVAT/static/js/main.c6281269.chunk.js"
+  },
+  {
+    "revision": "bd8da39e37d13980d2ad",
+    "url": "/AVAT/static/js/runtime-main.1ff5ca26.js"
+  }
+]);
