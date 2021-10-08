1bbbb461e48b5aca79ca968a720937a3efde4838
Pradeep Senthil <pradeepsen99@gmail.com>
2021-10-08 15:14:13 -0500

    Added segmentation functionality

--- src/tensorflow/SemanticSegmentation.js
+++ src/tensorflow/SemanticSegmentation.js
@@ -1,58 +1,58 @@
-import '@tensorflow/tfjs-backend-cpu';
-import '@tensorflow/tfjs-backend-webgl';
+//const tf = require('@tensorflow-models/tfjs-node');
+const deeplab = require('@tensorflow-models/deeplab');
+const tfconv = require('@tensorflow/tfjs-converter');
 
-import {load} from '@tensorflow-models/deeplab';
-import * as tf from '@tensorflow/tfjs-core';
+const fabric = require("fabric").fabric;
+const $ = require("jquery")
 
-import {BoundingBox} from '../annotations/bounding_box'
+const loadModel = async () => {
+    const modelName = 'pascal';   // set to your preferred model, either `pascal`, `cityscapes` or `ade20k`
+    const quantizationBytes = 4;  // either 1, 2 or 4
+    return await deeplab.load({base: modelName, quantizationBytes});
+  };
 
-let modelPromise;
 
-export async function load(model_type) {
-    alert("Loading model\nThis might take a second or so...")
-    const model = await cocoSsd.load();
-    model.dispose();
-    modelPromise = cocoSsd.load({base: model_type});
+export async function load(){
+
 }
 
-export async function run_model(fabricCanvas, annotation_data, currentFrame, save_data, refresh) {
+export async function run_model_segment(fabricCanvas, annotation_data, currentFrame, save_data, refresh) {
     const image = document.getElementsByTagName("video")[0]
     console.log(image.height)
 
-    console.log("IMAGE CLASSIFICATION")
-    const model = await modelPromise;
-    console.log('model loaded');
-    console.time('predict1');
-    const result = await model.detect(image);
-    console.timeEnd('predict1');
+    loadModel()
+    .then((model) => model.segment(image))
+    .then(
+        ({segmentationMap, width, height, legend}) =>{
 
-    for (let i = 0; i < result.length; i++) {
-        var box = result[i].bbox
-        var color = "#" + ((1<<24)*Math.random() | 0).toString(16)
-        var new_bbox = new BoundingBox(box[1], box[0], box[2], box[3], color, i+'b', "None").generate_no_behavior(fabricCanvas)
-        fabricCanvas.add(new_bbox)
-        annotation_data[currentFrame].push({id: i+'b', global_id: i,status: "", current: "", behavior: "", posture: "", notes: ""})
-    } 
-    save_data(currentFrame)
-    refresh()
-    /*
-    const c = document.getElementById('canvas');
-    const context = c.getContext('2d');
-    //context.drawImage(image, 0, 0);
-    context.font = '10px Arial';
+            console.log(segmentationMap);
+            console.log(fabricCanvas.getHeight())
+            console.log(fabricCanvas.getWidth())
+            //let width = fabricCanvas.getWidth();
+            //let height = fabricCanvas.getHeight();
+            var seg_image = new ImageData(segmentationMap, width, height)
+            //fabricCanvas.Image(seg_image)
 
-    console.log('number of detections: ', result.length);
-    for (let i = 0; i < result.length; i++) {
-        context.beginPath();
-        context.rect(...result[i].bbox);
-        context.lineWidth = 3;
-        context.strokeStyle = 'red';
-        context.fillStyle = 'red';
-        context.stroke();
-        context.fillText(
-            result[i].score.toFixed(3) + ' ' + result[i].class, result[i].bbox[0],
-            result[i].bbox[1] > 10 ? result[i].bbox[1] - 5 : 10);
-    }
-    console.log(result)
-    //alert(JSON.stringify(result))*/
-};
+            var c = document.createElement('canvas');
+
+            c.setAttribute('id', '_temp_canvas');
+            c.width = width;
+            c.height = height;
+
+            c.getContext('2d').putImageData(seg_image, 0, 0);
+
+            fabric.Image.fromURL(c.toDataURL(), function(img) {
+                img.left = 0;
+                img.top = 0;
+                img.opacity = .5;
+                img.scaleX = fabricCanvas.getHeight() / height;
+                img.scaleY = fabricCanvas.getWidth() / width;
+                fabricCanvas.add(img);
+                img.bringToFront();
+                c = null;
+                $('#_temp_canvas').remove();
+                fabricCanvas.renderAll();
+            });
+
+        });
+}
