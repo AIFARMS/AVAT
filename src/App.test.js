8192fb618802b328648f8202ff9d490320c9f492
Pradeep Senthil <pradeepsen99@gmail.com>
2021-10-11 23:46:11 -0500

    Testing first addition

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,46 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import SelectionScreen from '../src/ui_elements/Pages/selection_screen';
+import MainUpload from '../src/ui_elements/Pages/main_upload'
+import CustomNavBar from './ui_elements/Components/nav_bar';
+import AnnotationTable from './ui_elements/Components/change_table';
+
+import Button from 'react-bootstrap/Button'
+
+//========= SelectionScreen =========//
+
+test('Selection Screen - Video Upload', () => {
+  const SelectionScreen_test = render(<SelectionScreen/>);
+  expect(SelectionScreen_test.getByRole('button', {name: "Video Upload"}));
 });
+
+test('Selection Screen - Video Upload', () => {
+  const SelectionScreen_test = render(<SelectionScreen/>);
+  expect(SelectionScreen_test.getByRole('button', {name: "Multiview"}));
+});
+
+//========= CustomNavBar =========//
+
+test('CustomNavBar - Upload', () => {
+  const CustomNavBar_test = render(<CustomNavBar/>);
+  expect(CustomNavBar_test.getByRole('option', {name: "Upload"}));
+});
+
+test('CustomNavBar - Youtube', () => {
+  const CustomNavBar_test = render(<CustomNavBar/>);
+  expect(CustomNavBar_test.getByRole('option', {name: "Youtube"}));
+});
+
+/* test('CustomNavBar - EditSeg', () => {
+  const CustomNavBar_test = render(<CustomNavBar/>);
+  expect(CustomNavBar_test.getByRole('option', {name: "EditSeg"}));
+}); */
+
+//========= CustomNavBar =========//
+
+test('AnnotationTable - EditSeg', () => {
+  const AnnotationTable_test = render(<AnnotationTable/>);
+  expect(AnnotationTable_test.getByRole('option', {name: "EditSeg"}));
+});
+
