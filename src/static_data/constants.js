ec29025636e8d99510472fd6b30b0a59060aeaf7
Pradeep Senthil <pradeepsen99@gmail.com>
2021-12-21 17:31:33 -0600

    Update constants.js

--- src/static_data/constants.js
+++ src/static_data/constants.js
@@ -1,5 +1,4 @@
 export const ANNOTATION_FRAME = "1"
 export const ANNOTATION_BBOX = "2"
-export const ANNOTATION_KEYPOINT = "3"
-export const ANNOTATION_SEG = "4"
-
+export const ANNOTATION_SEG = "3"
+export const ANNOTATION_KEYPOINT = "4"
