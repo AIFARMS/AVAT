c638cab8ed4709cfdbdc6f65f32dac3e20499ba1
Pradeep Senthil <pradeepsen99@gmail.com>
2024-03-23 23:50:05 -0500

    Revert "Refactored upload modal to seperate component"
    This reverts commit be3fc29673072a4a312a5602d8dca27df9747d1d.

--- src/reducer/metadata.js
+++ src/reducer/metadata.js
@@ -2,7 +2,7 @@ import {createSlice} from '@reduxjs/toolkit'
 import {initFrameData, updateFrameData, getFrameData, initAnnotationData, updateAnnotationData, getAnnotationData, initColumnData, setMedia, initMedia} from '../processing/actions'
 
 
-const initialState = {horizontal_res: 0, vertical_res: 0, frame_rate: 1, media_type: "in_video", total_frames: 0, skip_value: null}
+const initialState = {horizontal_res: 0, vertical_res: 0, frame_rate: 1, media_type: "in_video", total_frames: 0, skip_value: 1}
 
 const metadataSlice = createSlice({
     name: 'metadata',
@@ -20,13 +20,13 @@ const metadataSlice = createSlice({
             state.vertical_res = payload.payload.vertical_res
         },
         setFrameRate(state, payload){
-	        // When the frame rate is changed, we first calculate the current frame rate and then multiply the total frames by the ratio of the new frame rate to the old frame rate
-            if (state.media_type != "in_image") { // only for videos
-                // var curr_framerate =  JSON.parse(JSON.stringify(state))['frame_rate']
-                // state.total_frames = state.total_frames / curr_framerate
-                // state.total_frames = state.total_frames * payload.payload.frame_rate
-                state.frame_rate = payload.payload.frame_rate
-            }
+	    // When the frame rate is changed, we first calculate the current frame rate and then multiply the total frames by the ratio of the new frame rate to the old frame rate
+	    if (state.media_type != "in_image") { // only for videos
+		    var curr_framerate =  JSON.parse(JSON.stringify(state))['frame_rate']
+		    state.total_frames = state.total_frames / curr_framerate
+		    state.total_frames = state.total_frames * payload.payload.frame_rate
+		    state.frame_rate = payload.payload.frame_rate
+	    }
         },
         setMediaType(state, payload){
             state.media_type = payload.payload.media_type
