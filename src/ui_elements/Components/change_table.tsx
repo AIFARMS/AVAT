a16ee558ac78ce8f1657029b873160927c60e454
Pradeep Senthil <pradeepsen99@gmail.com>
2026-05-07 21:41:23 -0500

    feat: remove "Current" tab

--- src/ui_elements/Components/change_table.tsx
+++ src/ui_elements/Components/change_table.tsx
@@ -1,7 +1,5 @@
 import React from "react"; 
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-
 import AnnotTable from './annot_table'
 
 import store from '../../store'
@@ -21,15 +19,9 @@ export default function AnnotationTable(props){
         <div className="flex h-full min-h-0 flex-col">
         {
             columns.length != 0 &&
-            <Tabs defaultValue="home" className="h-full min-h-0 gap-0">
-                <TabsList className="m-3 mb-0">
-                    <TabsTrigger value="home">Current</TabsTrigger>
-                </TabsList>
-                <TabsContent value="home" className="min-h-0 overflow-hidden p-3">
-				<AnnotTable columns={columns} data={annotation_data} select_data={select_data} current_frame={props.currentFrame} change_annot={props.handleChangeAnnot} selected_annotation_id={props.selectedAnnotationId}/>
-			</TabsContent>
-
-            </Tabs>
+            <div className="min-h-0 overflow-hidden p-3">
+                <AnnotTable columns={columns} data={annotation_data} select_data={select_data} current_frame={props.currentFrame} change_annot={props.handleChangeAnnot} selected_annotation_id={props.selectedAnnotationId}/>
+            </div>
         }   
         </div>
            
